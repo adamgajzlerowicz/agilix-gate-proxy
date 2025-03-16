689ea60ff661a67debd4e65f1832c6ddc013739d
Adam Gajzlerowicz <nelf86@gmail.com>
2025-03-16 14:34:31 +0100

    ...

--- index.js
+++ index.js
@@ -11,38 +11,38 @@ const deviceSocket = new WebSocket(deviceUrl)
 const browserClients = new Set()
 
 deviceSocket.on('message', msg => {
-  for (const client of browserClients) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(msg.toString())
+    for (const client of browserClients) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(msg.toString())
+        }
     }
-  }
 })
 
 deviceSocket.on("open", () => {
-  console.log('Gate connected')
+    console.log('Gate connected')
 })
 
 deviceSocket.on("close", () => {
-  console.log('Gate disconnected')
+    console.log('Gate disconnected')
 })
 
 wss.on('connection', browserSocket => {
-  console.log('Browser connected')
-  browserClients.add(browserSocket)
+    console.log('Browser connected')
+    browserClients.add(browserSocket)
 
-  browserSocket.on('message', msg => {
-    if (deviceSocket.readyState === WebSocket.OPEN) {
-      deviceSocket.send(msg)
-    }
-  })
+    browserSocket.on('message', msg => {
+        if (deviceSocket.readyState === WebSocket.OPEN) {
+            deviceSocket.send(msg)
+        }
+    })
 
-  browserSocket.on('close', () => {
-    console.log('Browser disconnected')
-    browserClients.delete(browserSocket)
-    if (!browserClients.size && deviceSocket.readyState === WebSocket.OPEN) {
-      deviceSocket.close()
-    }
-  })
+    browserSocket.on('close', () => {
+        console.log('Browser disconnected')
+        browserClients.delete(browserSocket)
+        if (!browserClients.size && deviceSocket.readyState === WebSocket.OPEN) {
+            deviceSocket.close()
+        }
+    })
 })
 
 server.listen(4326, () => console.log('Proxy server on port 4326'))
